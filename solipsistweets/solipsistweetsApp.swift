da1ea038fe25d53ff3c48354225aabd1d600e8f9
Chris Beiser <cbeiser@midjourney.com>
2025-08-31 15:42:15 -0700

    Initial Commit

---
+++ solipsistweets/solipsistweetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  solipsistweetsApp.swift
+//  solipsistweets
+//
+//  Created by Chris Beiser on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct solipsistweetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
